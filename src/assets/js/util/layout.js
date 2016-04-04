8406d5cc630e66e0764886c6e9701a90061d0601
jdhodges431 <jdhodges431@gmail.com>
2016-04-04 14:52:39 +0100

    Rounded secondaries height to prevent antialiasing issues

--- src/assets/js/util/layout.js
+++ src/assets/js/util/layout.js
@@ -3,8 +3,17 @@ import fc from 'd3fc';
 var renderedOnce = false;
 
 export default function(containers, charts) {
-    containers.secondaries.style('flex', charts.secondaries().charts().length);
-    containers.overlaySecondaries.style('flex', charts.secondaries().charts().length);
+    var primaryContainerFlex = 5;
+    var secondaryChartsShown = charts.secondaries().charts().length;
+
+    var secondariesHeight = Math.ceil((fc.util.innerDimensions(containers.charts.node()).height -
+        fc.util.innerDimensions(containers.xAxis.node()).height -
+        fc.util.innerDimensions(containers.navbar.node()).height) / (primaryContainerFlex + secondaryChartsShown));
+
+    var flexString = '0 0 ' + secondariesHeight * secondaryChartsShown + 'px';
+
+    containers.secondaries.style('flex', flexString);
+    containers.overlaySecondaries.style('flex', flexString);
 
     var headRowHeight = parseInt(containers.app.select('.head-row').style('height'), 10);
     if (!renderedOnce) {
